378352d6bf650cafdedea540159b9df7edaff939
Jay <jpca999@gmail.com>
2023-03-20 19:00:53 -0400

    added data to the model

--- src/main/java/com/ltp/gradesubmission/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -10,8 +10,17 @@ public class GradeController {
 	
 	@GetMapping("/")
 	public String sayHello() {
-		return "test"; 
+		System.out.println("sending test");
+		return "test";
 	}
 
+
+	@GetMapping("/grades")
+	public String getGrades(Model model) {
+		System.out.println("sending grades");
+		Grade grade = new Grade ( name: "Harry", subject: "English", score: "C-" );
+		model.addAttribute("grade", grade);
+		return "grades";
+	}
 
 }
